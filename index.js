2a55f88151a1fe87be5cb82d36deaa88be055e83
Christopher Harris <decode.harris@gmail.com>
2020-01-19 22:07:37 +1100

    modals
    created modals that are attached to the project display containers

--- index.js
+++ index.js
@@ -1,107 +1,255 @@
-const body = document.querySelector('.body');
+const section_content = document.querySelector('.content');
+const section_body = document.querySelector('.body');
+const section_nav = document.querySelector('.nav');
 
 let closeBtn = document.querySelector('.closeBtn');
 let modalContainer = document.querySelector('.modal_container');
 
+
+// project button variables
+const btn_case01 = document.querySelector('#btn_case01');
+const btn_case02 = document.querySelector('#btn_case02');
+const btn_case03 = document.querySelector('#btn_case03');
+const btn_case04 = document.querySelector('#btn_case04');
+// project container variables
+const project_01 = document.querySelector('#project_01');
+const project_02 = document.querySelector('#project_02');
+const project_03 = document.querySelector('#project_03');
+const project_04 = document.querySelector('#project_04');
+// project modal variables
+let modal_case01 = document.querySelector('#case_01');
+let modal_case02 = document.querySelector('#case_02');
+let modal_case03 = document.querySelector('#case_03');
+let modal_case04 = document.querySelector('#case_04');
+// position the project container with relative for attachment
+
+project_01.style.position = 'relative';
+project_02.style.position = 'relative';
+project_03.style.position = 'relative';
+project_04.style.position = 'relative';
+
 modalContainer.style.display = 'none';
 
-
-function modalView() {
-    projectView();
-
+// project button events
+btn_case01.addEventListener('click', () => {
     if (modalContainer.style.display == 'none') {
+       
+        properties_modalContainer();
+        properties_modalElements();
+        properties_hiddenNav();
         
-        // body.style.overflow = 'hidden';
-     
-        modalContainer.style.display = 'flex';
-        modalContainer.style.position = 'absolute';
-        modalContainer.style.top = window.scrollY;
-        modalContainer.style.left = '0';
+        modal_case01.style.display = 'grid';
+        modal_case02.style.display = 'none';
+        modal_case03.style.display = 'none';
+        modal_case04.style.display = 'none';
 
-        closeBtn.style.position = 'absolute';
-
-
-        closeBtn.style.fontSize = '2rem';
-        closeBtn.style.color = '#FFF';
-        closeBtn.style.background = 'trasnparent';
-        closeBtn.style.height = '80px';
-        closeBtn.style.width = '120px';
-
-        
-
+        // attach modal elements to modal container
+        modalContainer.appendChild(modal_case01);
+        // attach modal container to project 01
+        project_01.appendChild(modalContainer);
     } else {
-        body.style.overflow = 'auto';
         modalContainer.style.display = 'none';
     }
-}
-
-
-
-closeBtn.addEventListener('click', () => {
-    if (modalContainer.style.display = 'flex') {
-        body.style.overflow = 'auto';
-     
-        modalContainer.style.display = 'none';
+});
+btn_case02.addEventListener('click', () => {
+    if (modalContainer.style.display == 'none') {
+        // modal properties
+        properties_modalContainer();
+        properties_modalElements();
+        properties_hiddenNav();
+        
+        modal_case01.style.display = 'none';
+        modal_case02.style.display = 'grid';
+        modal_case03.style.display = 'none';
+        modal_case04.style.display = 'none';
+        
+        // attach modal elements to modal container
+        modalContainer.appendChild(modal_case02);
+        // attach modal container to project 02
+        project_02.appendChild(modalContainer);
     } else {
-        body.style.overflow = 'hidden';
-     
-        modalContainer.style.display = 'flex';
+        modalContainer.style.display = 'none';
+    }
+});
+btn_case03.addEventListener('click', () => {
+    if (modalContainer.style.display == 'none') {
+        // modal properties
+        properties_modalContainer();
+        properties_modalElements();
+        properties_hiddenNav();
+        
+        modal_case01.style.display = 'none';
+        modal_case02.style.display = 'none';
+        modal_case03.style.display = 'grid';
+        modal_case04.style.display = 'none';
+        
+        // attach modal elements to modal container
+        modalContainer.appendChild(modal_case03);
+        // attach modal container to project 03
+        project_03.appendChild(modalContainer);
+    } else {
+        modalContainer.style.display = 'none';
+    }
+});
+btn_case04.addEventListener('click', () => {
+    if (modalContainer.style.display == 'none') {
+        // modal properties
+        properties_modalContainer();
+        properties_modalElements();
+        properties_hiddenNav();
+        
+        modal_case01.style.display = 'none';
+        modal_case02.style.display = 'none';
+        modal_case03.style.display = 'none';
+        modal_case04.style.display = 'grid';
+        
+        // attach modal elements to modal container
+        modalContainer.appendChild(modal_case04);
+        // attach modal container to project 04
+        project_04.appendChild(modalContainer);
+    } else {
+        modalContainer.style.display = 'none';
     }
 });
 
+// properties function for modal container
+function properties_modalContainer() {
+        modalContainer.style.background = 'rgba(0,0,0,0.85)';
+
+        modalContainer.style.display = 'flex';
+        modalContainer.style.flexDirection = 'column';
+        modalContainer.style.justifyContent = 'center';
+        modalContainer.style.alignItems = 'center';
+        
+        modalContainer.style.position = 'absolute';
+        modalContainer.style.top = '0';
+        modalContainer.style.left = '0';
+        
+        modalContainer.style.height = '100%';
+        modalContainer.style.width = '100%';
+
+        section_body.style.overflow = 'hidden';
+        section_content.style.overflow = 'hidden';
+};
+
+// propertis function for modal elements
+function properties_modalElements() {
+    if (modal_case01.style.display == 'grid') {
+        modal_case01.style.size = '100%';
+        modal_case01.style.textAlign = 'center';
+        modal_case01.style.padding = '2%';
+    }
+    else if(modal_case02.style.display == 'grid') {
+        modal_case02.style.size = '100%';
+        modal_case02style.textAlign = 'center';
+        modal_case02.style.padding = '2%';
+    }
+    else if(modal_case03.style.display == 'grid') {
+        modal_case03.style.size = '100%';
+        modal_case03style.textAlign = 'center';
+        modal_case03.style.padding = '2%';
+    }
+    else if(modal_case04.style.display == 'grid') {
+        modal_case04.style.size = '100%';
+        modal_case04style.textAlign = 'center';
+        modal_case04.style.padding = '2%';
+    }
+};
+
+// function to close modal and revert container settings
+ function closeModal() {
+    if (modalContainer.style.display == 'flex') {
+        modalContainer.style.display = 'none';
+
+        project_01.style.overflow = 'auto';
+        section_body.style.overflow = 'auto';
+        section_nav.style.display = 'flex';
+        
+        modal_case01.style.display = 'none';
+        modal_case02.style.display = 'none';
+        modal_case03.style.display = 'none';
+        modal_case04.style.display = 'none';
+
+        console.log('exit button has been clicked');
+     
+        
+    } else {
+        section_body.style.overflow = 'hidden';
+        section_nav.style.display = 'none';
+        modalContainer.style.display = 'flex';
+    }
+  };
+
+
+// hidden navigation on modal appearance
+function properties_hiddenNav() {
+    if (section_nav.style.display == 'flex') {
+        section_nav.style.display = 'none';
+    } else {
+        section_nav.style.display = 'flex';
+    }
+};
 
 
 
-function projectView() {
-    let modalBtn = document.querySelectorAll('.modalBtn');
+
+
+
+
+
+
+
+
+// function projectView() {
+//     let modalBtn = document.querySelectorAll('.modalBtn');
     
 
 
-    var project_01 = document.querySelector('#project_01');
-    var project_02 = document.querySelector('#project_02');
-    var project_03 = document.querySelector('#project_03');
-    var project_04 = document.querySelector('#project_04');
+//     var project_01 = document.querySelector('#project_01');
+//     var project_02 = document.querySelector('#project_02');
+//     var project_03 = document.querySelector('#project_03');
+//     var project_04 = document.querySelector('#project_04');
 
-    if (modalBtn.id === 'modal_case01') {
-        project_01.style.display = 'grid';
-        project_02.style.display = 'none';
-        project_03.style.display = 'none';
-        project_04.style.display = 'none';
+//     if (modalBtn.id === 'modal_case01') {
+//         project_01.style.display = 'grid';
+//         project_02.style.display = 'none';
+//         project_03.style.display = 'none';
+//         project_04.style.display = 'none';
 
-        console.log('modal 01 is active');
-    }
-    else if (modalBtn.id === 'modal_case02') {
-        project_01.style.display = 'none';
-        project_02.style.display = 'grid';
-        project_03.style.display = 'none';
-        project_04.style.display = 'none';
+//         console.log('modal 01 is active');
+//     }
+//     else if (modalBtn.id === 'modal_case02') {
+//         project_01.style.display = 'none';
+//         project_02.style.display = 'grid';
+//         project_03.style.display = 'none';
+//         project_04.style.display = 'none';
 
-        console.log('modal 02 is active');
-    }
-    else if (modalBtn.id === 'modal_case03') {
-        project_01.style.display = 'none';
-        project_02.style.display = 'grid';
-        project_03.style.display = 'grid';
-        project_04.style.display = 'none';
+//         console.log('modal 02 is active');
+//     }
+//     else if (modalBtn.id === 'modal_case03') {
+//         project_01.style.display = 'none';
+//         project_02.style.display = 'grid';
+//         project_03.style.display = 'grid';
+//         project_04.style.display = 'none';
 
-        console.log('modal 03 is active');
-    }
-    else if (modalBtn.id === 'modal_case04') {
-        project_01.style.display = 'none';
-        project_02.style.display = 'none';
-        project_03.style.display = 'none';
-        project_04.style.display = 'grid';
+//         console.log('modal 03 is active');
+//     }
+//     else if (modalBtn.id === 'modal_case04') {
+//         project_01.style.display = 'none';
+//         project_02.style.display = 'none';
+//         project_03.style.display = 'none';
+//         project_04.style.display = 'grid';
 
-        console.log('modal 04 is active');
-    } else {
-        project_01.style.display = 'none';
-        project_02.style.display = 'none';
-        project_03.style.display = 'none';
-        project_04.style.display = 'none';
+//         console.log('modal 04 is active');
+//     } else {
+//         project_01.style.display = 'none';
+//         project_02.style.display = 'none';
+//         project_03.style.display = 'none';
+//         project_04.style.display = 'none';
 
-        console.log('no modal is active');
-    }
-};
+//         console.log('no modal is active');
+//     }
+// };
 
 
 
