4325c51cea6626f59913ef6ed71b51b4849257b2
Christopher Harris <decode.harris@gmail.com>
2020-03-22 20:31:24 +1100

    responsive css

--- gsap.js
+++ gsap.js
@@ -1,15 +1,30 @@
+ScrollReveal({reset : true});
+ScrollReveal().reveal(
+    // project props
+    gsap.timeline()
+        
+        .from('#projects', {x:200, opacity:0, duration: 1, ease:'back', delay: '5.5'})
+        .from('#tagline', {x:-200, opacity:0, duration: 1, ease:'back'}, '-=1')
+        .from('#showcase', {y:200, opacity:0, duration: 1.75, ease:'back'}, '-=0.75')
+        
+);
+
+ScrollReveal().reveal(
+    // skills props
+    gsap.timeline()
+    .from('#skills ul', {x:-200, stagger:0.7, opacity:0, duration: 1.85, ease:'back', delay:'7'})
+);
+
+
 // GSAP timeline animations
 gsap.timeline()
     // header props
     .from('#header', {duration: 1.2, opacity:0})
-    .from('#title', {opacity:0, scale:0, duration: 0.5, ease:'back'})
-    .from('#nav a', {y:200, stagger:0.2, opacity:0, duration: 0.9, ease:'back'})
+    .from('#title', {opacity:0, scale:0, duration: 0.7, ease:'back'})
+    .from('#nav a', {y:200, stagger:0.2, opacity:0, duration: 0.7, ease:'back'})
     // info props
-    .from('#info',{x:-200, stagger:0.1, opacity:0, duration: 0.5, ease:'back'})
-    // project props
-    .from('#projects', {x:200, opacity:0, duration: 1, ease:'back', delay: 2.1})
-    .from('#tagline', {x:-200, opacity:0, duration: 1, ease:'back'}, '-=1')
-    .from('#showcase', {y:200, opacity:0, duration: 1.8, ease:'back'}, '-=0.8')
+    .from('#info',{x:-200, stagger:0.1, opacity:0, duration: 0.9, ease:'back'});
+    
    
-    // skills props
-    .from('#skills ul', {x:-200, stagger:0.7, opacity:0, duration: 1, ease:'back', delay: 2});
+    
+    
