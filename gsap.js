9badb92494591e7411266cd1e1cb1850c816f000
Christopher Harris <decode.harris@gmail.com>
2020-03-22 22:06:39 +1100

    html clear

--- gsap.js
+++ gsap.js
@@ -4,26 +4,26 @@ gsap.timeline()
     .from('#header', {duration: 0.85, opacity:0})
     .from('#header h2', {opacity:0, scale:0, duration: 0.7, ease:'back'})
     .from('#header .tagline', {opacity:0, scale:0, duration: 0.7, ease:'back'})
-    // nav props
+    // // nav props
     .from('#nav a', {y:200, stagger:0.2, opacity:0, duration: 0.7, ease:'back'})
-    // info props
+    // // info props
     .from('#info',{xPercent:100, stagger:0.1, opacity:0, duration: 0.9, ease:'back'})
-    // projects props
+    // // projects props
     .from('#projects', {opacity:0, duration: 1})
     .from('#projects h3', {xPercent:-100, opacity:0, duration: 1, ease:'back'})
     .from('#projects .tagline', {xPercent:100, opacity:0, duration: 1, ease:'back'}, '-=1')
-    // showcase props
+    // // showcase props
     .from('#showcase', {yPercent:50, opacity:0, duration: 1, ease:'back'}, '-=1')
-    // skills props
+    // // skills props
     .from('#skills h3', {xPercent:100, opacity:0, duration: 1, ease:'back', delay: 1})
     .from('#skills .tagline', {xPercent:-100, opacity:0, duration: 1, ease:'back'}, '-=1')
-    // list props
+    // // list props
     .from('#skills ul', {yPercent:100, stagger:0.4, opacity:0, duration: 1, ease:'back', delay:'0.9'})
     // footer props
     .from('#footer', {duration: 0.85, opacity:0})
-    .from('#footer h3', {opacity:0, scale:0, duration: 0.7, ease:'back'})
-    .from('#footer .tagline', {opacity:0, scale:0, duration: 0.7, ease:'back'})
-    .from('#footer a', {yPercent:100, opacity:0, stagger: 0.5, ease:'back'})
+    .from('#footer h3', {xPercent:100, opacity:0, duration: 1, ease:'back'})
+    .from('#footer .tagline', {xPercent:-100, opacity:0, duration: 1, ease:'back'}, '-=1')
+    .from('#footer a', {duration: 1, yPercent:10, opacity:0, stagger: 0.5, ease:'back'})
 
 
     
