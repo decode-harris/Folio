0035886aa1c4964eff7b32895861693f08898bae
Christopher Harris <decode.harris@gmail.com>
2020-06-12 19:20:18 +1000

    restructure & formatt
    Restructure of HTML, CSS & JS

--- modal.js
+++ modal.js
@@ -1,81 +1,307 @@
-// selectors
-const body = document.querySelector('.body');
-const btn_connect = document.querySelector('.btn_connect');
+// application selector
+let body = document.querySelector('#body');
+let app = document.querySelector('#main');
+// modal selector
+let modal = document.querySelector('#modal');
+// modal item selectors
+let proj_name = document.querySelector('#proj_name');
+let proj_tag = document.querySelector('#proj_tag');
+let proj_desc = document.querySelector('#proj_desc');
+// project btn selectors
+let asgard = document.querySelector('#asgard');
+let alfheim = document.querySelector('#alfheim');
+let midgard = document.querySelector('#midgard');
+let muspellheim = document.querySelector('#muspellheim');
+let nidavellir = document.querySelector('#nidavellir');
+let vanaheim = document.querySelector('#vanaheim');
+let jotunheim = document.querySelector('#jotunheim');
+let helheim = document.querySelector('#helheim');
+// window position selector
+let value; 
 
-// btn event : connect modal
-btn_connect.addEventListener('click', ()=> {
-    // body props
-    body.style.position = 'relative';
-    body.style.overflow = 'hidden';
-
-    // modal
-    let modal = document.createElement('section');
-    modal.style.position = 'absolute';
-    modal.style.top = '0';
-    modal.style.left = '0';
-    modal.style.height = '100%';
-    modal.style.width = '100%';
-    modal.style.background = 'rgba(0,0,0,0.8)';
+// function : calculate window scroll position
+function calcWindowPosition() {
+    // assign value with scroll position
+    value = window.scrollY;
+    // test scroll position
+    console.log(value + 'px');
+}
+// event : asgard modal object
+asgard.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
     
-    modal.style.display = 'flex';
-    modal.style.flexDirection = 'column';
-    modal.style.justifyContent = 'center';
-    modal.style.alignItems = 'center';
-    modal.style.textAlign = 'center';
-
-    modal.style.maxHeight = '100vh';
-    modal.style.maxWidth = '100vw';
-
-    // modal title
-    let title = document.createElement('h3');
-    title.innerHTML = 'Connect';
-    title.style.color = '#FFF';
-
-    // modal info
-    let info = document.createElement('p');
-    info.innerHTML = 'Pleasse feel free to contact me through any of the following options';
-    info.style.color = '#FFF';
-
-    // btn modal close
-    let btn_close = document.createElement('button');
-    btn_close.innerHTML = 'x';
-
-    btn_close.style.position = 'absolute';
-    btn_close.style.top = '10px';
-    btn_close.style.right = '10px';
+    let desc = 
+            'A structured, responsive & customizable template ' +
+            'system. Developed to assist with quick implementation & visualization.';
     
-    btn_close.style.width = '100%';
-    btn_close.style.maxWidth = '180px';
+    if (proj_name.innerHTML !== 'Asgard') {
+        proj_name.innerHTML = 'Asgard';
+        proj_tag.innerHTML = 'Responsive Templating System';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
+});
+// event : alfheim modal object
+alfheim.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
     
-    btn_close.style.fontSize = '1.5rem';
-    btn_close.style.color = '#FFF';
-    btn_close.style.border = '2px solid #FFF';
-    btn_close.style.background = 'transparent';
-    btn_close.style.padding = '3%';
-
-    // btn_close.style.height = '40px';
-
-    // btn event : close
-    btn_close.addEventListener('click', ()=> {
-
-        if (modal.style.display == 'flex') {
-            modal.style.display = 'none';
-            body.style.overflow = 'auto';
-        } else {
-            modal.style.display = 'flex';
-            body.style.overflow = 'hidden';
-        }
-
-    });
-
+    let desc = 
+            'A fresh redesign, update & model of the current ' +
+            'website for the Marriott Hotel chain, Phuket Marriott.';
     
-    // append to modal
-    modal.appendChild(btn_close);
-    modal.appendChild(title);
-    modal.appendChild(info);
+    if (proj_name.innerHTML !== 'alfheim') {
+        proj_name.innerHTML = 'alfheim';
+        proj_tag.innerHTML = 'Luxury Travel';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
+
+});
+// event : midgard modal object
+midgard.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
     
+    let desc = 
+            'An expense tracking application to monitor ' +
+            '& calculate spending & savings. ' ;
+            
+    
+    if (proj_name.innerHTML !== 'midgard') {
+        proj_name.innerHTML = 'midgard';
+        proj_tag.innerHTML = 'Rental Expense Tracker';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
 
-    // append to body
-    body.appendChild(modal);
+});
+// event : muspellheim modal object
+muspellheim.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
+    
+    let desc = 
+            'A customizable mosaic gallery layout for ' +
+            'designers & photographers. ' ;
+            
+    
+    if (proj_name.innerHTML !== 'muspellheim') {
+        proj_name.innerHTML = 'muspellheim';
+        proj_tag.innerHTML = 'Photography Mosaic Portfolio';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
 
+});
+// event : niflheim modal object
+niflheim.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
+
+    let desc = 
+            'A fresh take on the traditional note taking application. ' +
+            'Add a grocery list, manage past lists & much more. ' ;
+            
+    
+    if (proj_name.innerHTML !== 'niflheim') {
+        proj_name.innerHTML = 'niflheim';
+        proj_tag.innerHTML = 'List Organisation Application';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
+
+});
+// event : nidavellir modal object
+nidavellir.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
+
+    let desc = 
+            ' ' +
+            ' ' +
+            '';
+    
+    if (proj_name.innerHTML !== 'nidavellir') {
+        proj_name.innerHTML = 'nidavellir';
+        proj_tag.innerHTML = 'Dashboard Management System';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
+
+});
+// event : vanaheim modal object
+vanaheim.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
+
+    let desc = 
+            ' ' +
+            ' ' +
+            '';
+    
+    if (proj_name.innerHTML !== 'vanaheim') {
+        proj_name.innerHTML = 'vanaheim';
+        proj_tag.innerHTML = 'Electron Desktop Application';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
+
+});
+// event : jotunheim modal object
+jotunheim.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
+
+    let desc = 
+            ' ' +
+            ' ' +
+            '';
+    
+    if (proj_name.innerHTML !== 'jotunheim') {
+        proj_name.innerHTML = 'jotunheim';
+        proj_tag.innerHTML = 'React Personal Planner';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
+
+});
+// event : helheim modal object
+helheim.addEventListener('click', ()=>{
+    // assign modal view & props
+    modalprops();
+
+    let desc = 
+            ' ' +
+            ' ' +
+            '';
+    
+    if (proj_name.innerHTML !== 'helheim') {
+        proj_name.innerHTML = 'helheim';
+        proj_tag.innerHTML = 'GSAP Animation Suite';
+    }
+    if (proj_desc.innerHTML !== desc.value) {
+        proj_desc.innerHTML = desc;
+    }
+    else {
+        proj_name.innerHTML = '';
+        proj_tag.innerHTML = '';
+        proj_desc.innerHTML = '';
+    }
+
+});
+
+// function : modal props
+function modalprops() {
+
+    // function : calculate window scroll position
+    calcWindowPosition();
+
+    // validate modal
+    if (modal.style.display == 'none') {
+        
+        // display modal object
+        modal.style.display = 'grid';
+        
+        // get value from calcWindowPosition()
+        modal.style.top = value + 'px';
+
+        // modal position props
+        modal.style.position = 'absolute';
+        modal.style.left = '0';
+        modal.style.height = '100%';
+        modal.style.maxHeight = '100%';
+        modal.style.maxWidth = '100%';
+        modal.style.width = '100%';
+        modal.style.background = 'rgba(0,0,0,0.5)';
+        modal.style.zIndex = '1000';
+
+        // modal grid positioning
+        modal.style.gridColumn = 'span 6';
+        modal.style.gridRow = 'span 2';
+        
+        // apply overflow hidden to body when modal is active
+        body.style.overflow = 'hidden';
+        
+        // test modal value
+        console.log(value);
+
+        // test modal props
+        console.log('modal styles applied');
+        
+    }
+    else {
+        // revert modal to default style
+        modal.style.display = 'none';
+        body.style.overflow = 'auto';
+    }
+}
+
+// exit btn selector
+let btn_exit = document.querySelector('#btn_exit');
+// event : btn exit | close modal
+btn_exit.addEventListener('click', ()=>{
+
+    if (modal.style.display == 'grid') {
+        modal.style.display = 'none';
+
+        // test modal props removed
+        console.log('modal props removed');
+    }
+    if (body.style.overflow == 'hidden') {
+        body.style.overflow = 'scroll';
+    }
+    else {
+        modal.style.display = 'grid';
+        body.style.overflow = 'scroll';
+    }
 });
