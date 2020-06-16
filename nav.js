2f797671621175e08560f93348b671f3431f16cc
Christopher Harris <decode.harris@gmail.com>
2020-06-16 18:44:25 +1000

    header restructure
    neaten up code for header component

--- nav.js
+++ nav.js
@@ -6,7 +6,7 @@ let navlist = document.querySelector('#navlist');
 
 // nav prop selectors
 let nav_btn = document.querySelector('#nav_btn');
-let nav_title = document.querySelector('#nav_title');
+// let nav_title = document.querySelector('#nav_title');
 let nav_ul = document.querySelector('#nav_ul');
 
 
@@ -20,27 +20,16 @@ function navproperties() {
         nav_btn.style.display = 'flex';
         // test : navbtn container = display none
         console.log('nav btn cont = display : none')
-
-        nav_title.style.display = 'none';
-        // test : nav title = display none
-        console.log('nav title = display : none');
-
+        // assign nav list to display none
         nav_ul.style.display = 'none';
         // test : nav ul = display none
         console.log('nav ul = display : none');
-
     }
     else {
         // assign default hidden values to nav elemnts on mobile
-        nav_title.style.display = 'flex';
         nav_ul.style.display = 'flex';
         nav_btn.style.display = 'none';
-
     }
-    
-    
-   
-    
 }
 
 // event : nav btn
@@ -51,14 +40,7 @@ navbtn.addEventListener('click', ()=>{
 
         console.log('nav ul : display = flex');
     }
-    if (nav_title.style.display == 'none' || nav_title.style.display === null) {
-        nav_title.style.display = 'flex';
-
-        console.log('nav title : display = flex');
-    }
-    
     else {
-        nav_title.style.display = 'none';
         nav_ul.style.display = 'none';
 
         console.log('nav btn : default props');
